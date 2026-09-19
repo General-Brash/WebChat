cdce5da6c3d33e58dec3a0d9834388da97c637d3
General-Brash <3256234224@qq.com>
2026-09-19 22:22:01 +0800

    chore: regenerate API contracts for v1.1.0

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -28640,7 +28640,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.4.1",
+	Version:          "1.1.0",
 	Host:             "",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
